dfdecd627d756a81fbfc31d7552a76ff396ebb62
Rishit Dagli <rishit.dagli@gmail.com>
2021-02-15 12:56:50 +0530

    Initial package commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gradient-centralization-tf",
+    version="0.0.1",
+    description=" ",
+    py_modules=["gradient-centralization-tf"],
+    package_dir={"": "src"},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    url="https://github.com/Rishit-dagli/Gradient-Centralization-TensorFlow",
+    author="Rishit Dagli",
+    author_email="rishit.dagli@gmail.com",
+
+    # extras_require={
+    #     "dev": [
+    #         "pytest >= 3.7",
+    #         "check-manifest",
+    #         "twine",
+    #     ],
+    # },
+)
