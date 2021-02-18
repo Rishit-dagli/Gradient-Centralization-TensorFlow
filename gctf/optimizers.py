f88df82ad07528509e6f931ce101a76fcbd94e0b
Rishit-dagli <Rishit-dagli@users.noreply.github.com>
2021-02-18 11:14:45 +0000

    autopep8 action fixes

--- gctf/optimizers.py
+++ gctf/optimizers.py
@@ -8,7 +8,11 @@ def adam(learning_rate=0.001,
          beta_2=0.999,
          epsilon=1e-7,
          amsgrad=False):
-    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=beta_1, beta_2=beta_2, epsilon=epsilon,
-                                         amsgrad=amsgrad)
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate=learning_rate,
+        beta_1=beta_1,
+        beta_2=beta_2,
+        epsilon=epsilon,
+        amsgrad=amsgrad)
     optimizer.get_gradients = centralized_gradients_for_optimizer(optimizer)
     return optimizer
