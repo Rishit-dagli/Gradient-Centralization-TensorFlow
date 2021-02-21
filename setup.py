fe87aad91c414b95208d34423422620a37dfc198
Rishit Dagli <rishit.dagli@gmail.com>
2021-02-21 13:30:13 +0530

    Update setup.py to now support multiple modules

--- setup.py
+++ setup.py
@@ -9,9 +9,11 @@ setup(
     name="gradient-centralization-tf",
     version="0.0.1",
     description="Implement Gradient Centralization in TensorFlow",
-    py_modules=["gctf"],
+    packages=["gctf"],
+
     long_description=long_description,
     long_description_content_type="text/markdown",
+
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
@@ -20,13 +22,16 @@ setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "License :: OSI Approved :: Apache Software License",
     ],
+
     url="https://github.com/Rishit-dagli/Gradient-Centralization-TensorFlow",
     author="Rishit Dagli",
     author_email="rishit.dagli@gmail.com",
+
     install_requires=[
         "tensorflow ~= 2.4.0",
         "keras ~= 2.4.0",
     ],
+
     extras_require={
         "dev": [
             "check-manifest",
